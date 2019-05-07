62af85dcc6b23265cb04cb2cf95c0b91ba7143e5
ropeks <ropeksmilioner@gmail.com>
2019-05-07 14:19:28 +0200

    do Challenge 3 in arrays.js

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -92,8 +92,12 @@ console.log(lcar["car_make"], lcar["car_model"]);
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
-let carModels = [];
-console.log();
+let carModels = inventory.sort(function(a, b){
+    if(a.car_model < b.car_model) { return -1; }
+    if(a.car_model > b.car_model) { return 1; }
+    return 0;
+});
+console.log(carModels);
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
